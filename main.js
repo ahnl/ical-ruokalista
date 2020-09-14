260f987aef1b6caa23410a5b47450b717c2885a7
ahnl <you@example.com>
2020-09-14 11:27:35 +0300

    Added winston logging

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 const ical = require('ical-generator');
 const express = require('express');
 const app = express();
-const {dateWithTime} = require('./utils.js');
+const { dateWithTime } = require('./utils.js');
+const { createLogger, format, transports } = require('winston');
+const logdnaWinston = require('logdna-winston');
+
 const ruokalista = {
     "jamix": require('./services/jamix.js')
 }
 
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        format.errors({stack: true}),
+        format.splat(),
+        format.json()
+    ),
+    transports: [
+        new transports.File({filename: 'error.log', level: 'error'}),
+        new transports.File({filename: 'combined.log'}),
+        new transports.Console({format: format.combine(format.colorize(), format.simple())})
+    ]
+});
+
+if (process.env.logdnaToken) {
+    logger.add(new logdnaWinston({
+        key: process.env.logdnaToken.toString().trim(),
+        app: 'ical-ruokalista',
+        index_meta: true
+    }));
+    logger.info('[LOGGING] Added logdna transport!');
+}
+
 function createCalendar(service, query) {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         try {
             ruokalista[service](query).then(data => {
                 const cal = ical({
@@ -15,7 +42,7 @@ function createCalendar(service, query) {
                     name: 'Ruokalista - ' + data.title,
                     timezone: 'Europe/Helsinki'
                 });
-        
+
                 for (let day in data.menu) {
                     const event = cal.createEvent({
                         start: dateWithTime(day, ...query.start.split(':')),
@@ -35,7 +62,9 @@ function createCalendar(service, query) {
 }
 
 app.get('/ics/:service', (req, res, next) => {
-    console.log(req.params.service, req.query)
+    const ip = (req.headers['cf-connecting-ip'] || req.connection.remoteAddress);
+    const meta = {...req.query, ip: ip, userAgent: req.headers['user-agent']};
+    logger.info('[HTTP] Requested service ' + req.params.service + ' by ' + ip, {...meta})
     try {
         createCalendar(req.params.service, req.query).then(ics => {
             res.writeHead(200, {
@@ -44,13 +73,15 @@ app.get('/ics/:service', (req, res, next) => {
             });
             res.end(ics, 'utf8');
         }).catch(reason => {
-            res.status(500).end('Bad Request');            
+            res.status(500).end('Bad Request');
+            logger.error(reason, {...meta});
         });
     } catch (e) {
         res.status(500).end('Internal Server Error');
+        logger.error(e, {...meta});
     }
-
 })
 
-app.listen(80);
-
+app.listen(80, () => {
+    logger.info('[HTTP] Server ready!');
+});
